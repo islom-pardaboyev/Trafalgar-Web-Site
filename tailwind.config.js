66ccc4f54975202d1b0c8ac3fea02913172916e7
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-03-02 22:37:22 +0500

    Updated File

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'link-color': '#1F1534',
+      'gray': '#7D7987',
+      'blue': '#458FF6',
+      'black': '#000000'
+    },
+    fontFamily: {
+      'mulish': ["Mulish", "sans-serif"]
+    }
   },
   plugins: [],
 }
